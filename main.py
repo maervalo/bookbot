9dbfc88e362b35eac0b58869c91d6f810fc111aa
maervalo <maervalo@gmail.com>
2025-04-14 22:39:51 +0200

    0414.2: print book from file

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("greetings boots")
+def get_book_text(filepath):
+    with open(filepath) as f:
+        file_contents = f.read()
+    return file_contents
+
+def main():
+    entire_content = get_book_text("./books/frankenstein.txt")
+    print(entire_content)
+
+main()
