bc2ab2edc00cedfe7ba7b320348d6c1a8990004e
maervalo <maervalo@gmail.com>
2025-04-15 17:22:46 +0200

    0415.2: sys.argv pointing to filepath

--- main.py
+++ main.py
@@ -1,16 +1,29 @@
+import sys
 from stats import get_num_words
 from stats import get_char_count
 from stats import sort_chars
 
 def get_book_text(filepath):
-    with open(filepath) as f:
-        file_contents = f.read()
-    return file_contents
-
+    try:
+          with open(filepath, 'r') as f:
+               return f.read()
+    except FileNotFoundError:
+         print(f"Error: File not found - {filepath}")
+         return None
+    
 def main():
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    
+    book_path = sys.argv[1]
+    book_text = get_book_text(book_path)
+    
+    if not book_text:
+        sys.exit(1)
     print("============ BOOKBOT ============")
-    print("Analyzing book found at books/frankenstein.txt...")
-    entire_content = get_book_text("./books/frankenstein.txt")
+    print(f"Analyzing book found at {book_path}")
+    entire_content = get_book_text(book_path)
     print("----------- Word Count ----------")
     number_of_words = get_num_words(entire_content)
     print(f"Found {number_of_words} total words")
